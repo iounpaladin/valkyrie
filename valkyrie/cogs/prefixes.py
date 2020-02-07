124c0eb71a2047156469f9e96c8e553b248736ce
Max Steinberg <maxssteinberg@gmail.com>
2020-02-06 19:19:27 -0800

    fucking bullshit II

--- valkyrie/cogs/prefixes.py
+++ valkyrie/cogs/prefixes.py
@@ -1,4 +1,6 @@
+import discord
 from discord.ext import commands
+from discord.ext.commands import CommandInvokeError
 
 from valkyrie.data import default_prefixes, custom_prefixes
 
@@ -9,6 +11,12 @@ class Prefixes(commands.Cog):
     @commands.has_permissions(manage_guild=True)  # Only mods can use this
     async def prefix(self, ctx, *, prefixes=""):
         """Sets the prefix(es) of this bot in this guild"""
+        me: discord.Member = ctx.me
+        try:
+            await ctx.guild.me.edit(nick=me.name + f' [{", ".join(prefixes.split() or default_prefixes)}]')
+        except:
+            return await ctx.send("Failed! Is that prefix too long?")
+
         custom_prefixes.set(ctx.guild.id, prefixes.split() or default_prefixes)
         await ctx.send("Prefixes set!")
 
